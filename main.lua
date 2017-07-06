ae4bcfdccf4e3889b51b73451c42e02c191f6308
Flora Rosenkreuz <flora@ranyakumo.xyz>
2017-07-06 15:14:24 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,24 @@
+BasePart = script.Parent.BasePart
+
+http = game:GetService("HttpService")
+
+response = http:GetAsync(script.Parent.URL.Value, true)
+
+image = http:JSONDecode(response)
+
+response = nil
+
+BasePosition = BasePart.Position
+RowStart = Vector3.new(BasePosition.X, BasePosition.Y - 0.2, BasePosition.Z)
+BasePosition = RowStart
+for index, row in ipairs(image) do
+	for index2, pixel in ipairs(row) do
+		part = BasePart:Clone()
+		part.Parent = script.Parent
+		part.Position = BasePosition
+		part.Color = Color3.fromRGB(pixel[0], pixel[1], pixel[2])
+		BasePosition = Vector3.new(BasePosition.X + 0.2, BasePosition.Y, BasePosition.Z)
+	end
+	RowStart = Vector3.new(RowStart.X, RowStart.Y - 0.2, RowStart.Z)
+	BasePosition = RowStart
+end
