f918d0362bff332d0c9977c59a2a048f25646c13
Flora Rosenkreuz <flora@ranyakumo.xyz>
2017-07-06 19:33:40 -0500

    are you fucking kidding me why do tables start at 1

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ for index, row in ipairs(image) do
 		part = BasePart:Clone()
 		part.Parent = script.Parent
 		part.Position = BasePosition
-		part.Color = Color3.fromRGB(pixel[0], pixel[1], pixel[2])
+		part.Color = Color3.fromRGB(pixel[1], pixel[2], pixel[3])
 		BasePosition = Vector3.new(BasePosition.X + 0.2, BasePosition.Y, BasePosition.Z)
 	end
 	RowStart = Vector3.new(RowStart.X, RowStart.Y - 0.2, RowStart.Z)
